5ee7885fcaac800685abceac13eb6977681fd4d6
kkan0615 <kkan0615@gmail.com>
2022-07-11 20:52:47 -0400

    :zap: Feat: add tailwind colors

---
+++ tailwind.config.js
@@ -0,0 +1,110 @@
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+  ],
+  prefix: 'tw-',
+  theme: {
+    extend: {
+      colors: {
+        'primary': {
+          '100': {
+            light: '#0455BF',
+            DEFAULT: '#0455BF',
+            dark: '#0455BF',
+          },
+          '200': {
+            light: '#0455BF',
+            DEFAULT: '#0455BF',
+            dark: '#0455BF',
+          },
+          '300': {
+            light: '#0455BF',
+            DEFAULT: '#0455BF',
+            dark: '#0455BF',
+          },
+          '400': {
+            light: '#0455BF',
+            DEFAULT: '#0455BF',
+            dark: '#0455BF',
+          },
+          '500': {
+            light: '#0455BF',
+            DEFAULT: '#0455BF',
+            dark: '#0455BF',
+          },
+          '600': {
+            light: '#0455BF',
+            DEFAULT: '#0455BF',
+            dark: '#0455BF',
+          },
+          '700': {
+            light: '#0455BF',
+            DEFAULT: '#0455BF',
+            dark: '#0455BF',
+          },
+          '800': {
+            light: '#0455BF',
+            DEFAULT: '#0455BF',
+            dark: '#0455BF',
+          },
+          '900': {
+            light: '#0455BF',
+            DEFAULT: '#0455BF',
+            dark: '#0455BF',
+          }
+        }, // primary ends
+        'secondary': {
+          '100': {
+            light: '#766A5B',
+            DEFAULT: '#766A5B',
+            dark: '#766A5B',
+          },
+          '200': {
+            light: '#766A5B',
+            DEFAULT: '#766A5B',
+            dark: '#766A5B',
+          },
+          '300': {
+            light: '#766A5B',
+            DEFAULT: '#766A5B',
+            dark: '#766A5B',
+          },
+          '400': {
+            light: '#766A5B',
+            DEFAULT: '#766A5B',
+            dark: '#766A5B',
+          },
+          '500': {
+            light: '#766A5B',
+            DEFAULT: '#766A5B',
+            dark: '#766A5B',
+          },
+          '600': {
+            light: '#766A5B',
+            DEFAULT: '#766A5B',
+            dark: '#766A5B',
+          },
+          '700': {
+            light: '#766A5B',
+            DEFAULT: '#766A5B',
+            dark: '#766A5B',
+          },
+          '800': {
+            light: '#766A5B',
+            DEFAULT: '#766A5B',
+            dark: '#766A5B',
+          },
+          '900': {
+            light: '#766A5B',
+            DEFAULT: '#766A5B',
+            dark: '#766A5B',
+          }
+        }, // secondary ends
+      }, // colors end
+    },
+  },
+  plugins: [],
+}
