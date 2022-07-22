b2b9d9134e0b8725faafbb115edaaf335ad4a042
kkan0615 <kkan0615@gmail.com>
2022-07-22 15:31:27 -0400

    :zap: Feat: add pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,32 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'url'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue({
       reactivityTransform: false
+    }),
+    VitePWA({
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: 'vue3-boilerplate',
+        short_name: 'boilerplate',
+        start_url: './',
+        description: 'boilerplate!',
+        theme_color: '#41B883',
+        display: 'standalone',
+        icons: [
+          {
+            src: './favicon.ico',
+            sizes: '32x32',
+          },
+        ]
+      },
     })
   ],
   server: {
