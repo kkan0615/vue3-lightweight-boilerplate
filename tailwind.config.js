6a5af31cca0054ea496076ec3ce39a19c27887d9
kkan0615 <kkan0615@gmail.com>
2022-09-06 10:21:02 -0400

    :zap: Feat: add typescript eslint

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-// eslint-disable-next-line no-undef
+// eslint-disable-next-line no-undef, @typescript-eslint/no-var-requires
 const colors = require('tailwindcss/colors')
 
 /** @type {import('tailwindcss').Config} */
